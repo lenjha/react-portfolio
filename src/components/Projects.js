97da421036a8920e7baac8e5ad8efc85d9b54660
Lena Ha <vermilionha@gmail.com>
2018-06-18 13:04:49 -0700

    add projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,33 +6,37 @@ class Projects extends Component {
   constructor() {
     super();
     this.state = {
-      projects: [
-        {
-          title: 'optimo',
-          link: 'http://michaelyoungrealtor.net/#/',
-          date: '2018',
-          category: 'SCSS, VueJS'
-        },
-        {
-          title: 'animal town',
-          link: 'https://github.com/lenjha/animal-town',
-          date: '2018',
-          category: 'HTML, SCSS, JS'
-        },
-        {
-          title: 'post-apocalyptia',
-          link: 'https://lenjha.github.io/welcome-to-post-apocalyptia/',
-          date: '2017',
-          category: 'HTML, CSS, JS'
-        },
-        {
-          title: 'trackertron',
-          link: 'https://lenjha.github.io/trackertron/',
-          date: '2017',
-          category: 'HTML, CSS, JS'
-        }
-      ]
+      projects: []
     }
+  }
+
+  componentWillMount(){
+    this.setState({projects: [
+      {
+        title: 'optimo',
+        link: 'http://michaelyoungrealtor.net/#/',
+        date: '2018',
+        category: 'SCSS, VueJS'
+      },
+      {
+        title: 'animal town',
+        link: 'https://github.com/lenjha/animal-town',
+        date: '2018',
+        category: 'HTML, SCSS, JS'
+      },
+      {
+        title: 'post-apocalyptia',
+        link: 'https://lenjha.github.io/welcome-to-post-apocalyptia/',
+        date: '2017',
+        category: 'HTML, CSS, JS'
+      },
+      {
+        title: 'trackertron',
+        link: 'https://lenjha.github.io/trackertron/',
+        date: '2017',
+        category: 'HTML, CSS, JS'
+      }
+    ]});
   }
 
   render() {
